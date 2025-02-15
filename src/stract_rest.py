79e9a34ba9cf47e00b9264b6934d0b995b028f1a
fernando-pacheco <fernandopachecopx@gmail.com>
2025-02-15 03:23:50 -0300

    feat: método para retornar dados paginados

--- src/stract_rest.py
+++ src/stract_rest.py
@@ -14,3 +14,24 @@ class StractRest:
             return response.json()
         except requests.RequestException as e:
             return {"error": str(e)}
+
+    def fetch_paginated_data(self, endpoint, params=None, key=None):
+        """Percorre todas as páginas da API e retorna todos os elementos."""
+        all_items = []
+        page = 1
+        total_pages = 1
+        
+        while page <= total_pages:
+            if params is None:
+                params = {}
+            params["page"] = page
+            
+            response = self.fetch_data(endpoint, params)
+            if key and key in response:
+                all_items.extend(response[key])
+            
+            pagination = response.get("pagination", {})
+            total_pages = pagination.get("total", total_pages)
+            page += 1
+            
+        return all_items
